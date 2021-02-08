1c455cd049f5ddd8213ec2c1fee0231391604993
Mykyta Bashenko <45737720+MykytaBAshenko@users.noreply.github.com>
2021-02-08 11:10:40 +0200

    Update task4.c

--- 3laboratory_2variant/task4.c
+++ 3laboratory_2variant/task4.c
@@ -5,30 +5,7 @@
 #include <limits.h>
 #include <math.h>
  #include <stdio.h>
-// double recursion (int k ,int n,double ans) {
-//     if(k <= n) {
-//         ans += (1.0 / (( 2 * k -1)*(2 * k +1)) );
-//         k++;
-//         return recursion(k , n, ans);
-//     }
-//     return ans;
-// }
 
-
-// double cickle (int n) {
-//     double ans = 0;
-//     for  (int k = 1; k <= n; k++ ) {
-//         ans += (1.0 / (( 2 * k -1)*(2 * k +1)) );
-//     }
-//     return ans;
-// }
- 
-// int main(int argc, char **argv) {
-//     double recur = recursion(1,atoi(argv[1]),0.0);
-//     double cickl = cickle(atoi(argv[1]));
-//     printf("цикл %f\n",cickl);
-//     printf("рекурсия %f\n",recur);
-// }
 
 void arrgen(int n, int *nn) {
     for(int i=0;i<n;i++)
